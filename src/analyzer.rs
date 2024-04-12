d567e87a1be37a1222e5a5b11a754a6649f03a4a
David Schramm <david.schramm@live.com>
2024-04-12 13:45:30 +0200

    more

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -1,6 +1,22 @@
-pub enum SampleType {
+pub enum Sample {
     Mail(Vec<u8>),
     Raw(Vec<u8>),
 }
 
+pub enum AnalysisError {
+    NotValid,
+}
+
+pub struct AnalysisResult {}
+
 pub struct Analyzer {}
+
+impl Analyzer {
+    pub fn new() -> Self {
+        Analyzer {}
+    }
+
+    pub fn analyze(&self, sample: Sample) -> Result<AnalResult, AnalError> {
+        Ok(AnalysisResult {})
+    }
+}
